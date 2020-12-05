b78ef70ccaebd6b91e2bc9a714596ef06dfa7943
Saiyam Pathak <saiyampathak@Saiyams-MacBook-Pro.local>
2020-12-05 17:58:01 +0530

    adding ingress and few changes

--- src/main.py
+++ src/main.py
@@ -11,10 +11,10 @@ def influx():
     org = "test"
     bucket = "test"
     print("{}".format(os.environ.get('token')))
-    client = InfluxDBClient(url="http://185.136.234.159:30305", token="{}".format(os.environ.get('token')))
-
+    client = InfluxDBClient(url="{}".format(os.environ.get('host')), token="{}".format(os.environ.get('token')))
+    
     write_api = client.write_api(write_options=SYNCHRONOUS)
-    point = Point("mem").tag("host", "host1").field("used_percent", 23.43234543).time(datetime.utcnow(), WritePrecision.NS)
+    point = Point("mem").tag("host", socket.gethostname()).field("used_percent", 23.43234543).time(datetime.utcnow(), WritePrecision.NS)
     write_api.write(bucket, org, point)
 
 schedule.every(5).seconds.do(influx)   
